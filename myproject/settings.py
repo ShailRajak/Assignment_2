a36175568cc15e37ed7e199f4b89e9b0ff963a9e
ShailRajak <shailendrarajak788@gmail.com>
2025-12-11 12:26:02 +0530

    fix mysql backend

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,7 +78,7 @@ import dj_database_url
 import os
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('MYSQL_URL'))
+    'default': dj_database_url.config(default=os.environ.get('MYSQL_URL'), engine='django.db.backends.mysql')
 }
 
 
