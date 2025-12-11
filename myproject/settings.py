d13254d47562cb6353099fc2d7bb151b31f59bfa
ShailRajak <shailendrarajak788@gmail.com>
2025-12-11 13:07:55 +0530

    remove engine override fix crash

--- myproject/settings.py
+++ myproject/settings.py
@@ -83,10 +83,6 @@ DATABASES = {
     )
 }
 
-# Force MySQL backend using PyMySQL
-DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
